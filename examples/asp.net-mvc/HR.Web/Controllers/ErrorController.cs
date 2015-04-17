25b685bc052b83850e496e1b4b6545d24bad78ca
Sandrino Di Mattia <contact@sandrino.be>
2015-04-17 15:06:31 +0200

    Improved unauthorized suppport in sample app

---
+++ examples/asp.net-mvc/HR.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Unauthorized()
+        {
+            return View("Unauthorized");
+        }
+    }
+}
